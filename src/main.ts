18e588f5667d79a80e201ed4fa8fc5a1c174ab03
@FoxMykyta <nick_durnev@pragmasoft.com.ua>
2023-10-05 19:38:12 +0300

    feat: changes for deploy

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 
-const PORT = 3000;
-
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  const PORT = process.env.PORT;
   await app.listen(PORT);
   logger.log(`Application is running on port ${PORT}`);
 }
